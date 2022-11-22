62143dad0e91bb842bcbc89eac59f12b8e7ee160
L3on06 <l3onshabani@gmail.com>
2022-11-22 00:40:20 -0800

    I added the score board and fix some bugs.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
+import ScoreBoard from './components/ScoreBoard';
 
 function App() {
 
@@ -17,6 +18,9 @@ function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setPlaying] = useState(true);
+  const [scores, setScores] = useState({ xScore: 0, oScore: 0 });
+  const [gameOver, setGameOver] = useState(false);
+
 
   const handleBoxClick = (boxIdx) => {
     const updateBoard = board.map((value, idx) => {
@@ -26,7 +30,21 @@ function App() {
         return value;
       }
     })
-    checkWinner(updateBoard);
+    const winner = checkWinner(updateBoard);
+
+    if (winner) {
+      if (winner === "O") {
+        let { oScore } = scores;
+        oScore += 1
+        setScores({ ...scores, oScore })
+      } else {
+        let { xScore } = scores;
+        xScore += 1
+        setScores({ ...scores, xScore })
+      }
+    }
+
+    console.log(scores)
 
     setBoard(updateBoard);
     setPlaying(!xPlaying);
@@ -38,15 +56,22 @@ function App() {
       const [x, y, z] = WIN_CONDITIONS[i];
 
       if (board[x] && board[x] === board[y] && board[y] === board[z]) {
-        console.log(board[x])
+        setGameOver(true)
         return board[x];
       }
     }
   }
 
+  const resetBoard = () => {
+    alert("The " + checkWinner(board) + " Player Win")
+    setGameOver(false);
+    setBoard(Array(9).fill(null))
+  }
+
   return (
     <div className="App">
-      <Board board={board} onClick={handleBoxClick} />
+      <ScoreBoard scores={scores} xPlaying={xPlaying} />
+      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick} />
     </div>
   );
 }
