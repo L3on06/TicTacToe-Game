8a0b91502bdf5a7f7c0f9265e7ead68f1667271c
L3on06 <l3onshabani@gmail.com>
2022-12-08 01:37:31 -0800

    I added custom cursor and made it dynamic.

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -5,6 +5,6 @@ export const Box = ({ value, onClick }) => {
     const style = value === "X" ? "box x" : "box o";
 
     return (
-        <button className={style} onClick={onClick}>{value}</button>
+        <button className={style} style={value ? { cursor: 'url("cursorPointerDone.svg"), pointer' } : { cursor: 'url("cursorPointerStart.svg"), pointer' }} onClick={onClick}>{value}</button>
     )
 }
